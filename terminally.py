28e6d808ba50e50ae5b106f7b516b8d31805e66a
Daniel Lake <90655952+laked0601@users.noreply.github.com>
2022-05-29 21:14:08 +0100

    Update terminally.py
    Terminally has now been updated to be an imported module, please see README.md for explanation

--- terminally.py
+++ terminally.py
@@ -1,65 +1,67 @@
 from tkinter import Tk, ttk, Frame, Entry, X, Y, BOTH, LEFT, RIGHT, BOTTOM, TOP, PhotoImage, Button
-from types import FunctionType
-
-CLASSES = {}
-# {COMPILE FROM HERE}
-# CLASSES = {'compiled_class_name':classobj, 'other_compiled_class_name':classobj, ...)
-APPLICATION_NAME = "{Your Application Name}"
-PROCEDURES=[
-    {"name":"test.py",
-    "icon":"favicon.png",
-    "proc":"myclass",
-     },
-    {"name": "name_ager.py",
-     "proc":"name_ager"
-     },
-    {"name": "hello"}
-]
-
 class base_window(Tk):
     class toptab_class():
-        def __init__(self, rootobj, application_name, procedures=[]):
+        def __init__(self, rootobj):
             self.rootobj = rootobj
             self.toptab = Frame(rootobj)
-            self.nameframe = Frame(self.toptab, bg="#90fcad", height=32)
-            tt_name = ttk.Label(self.nameframe, text=application_name, font=("Arial", 18))
-            tt_name.place(x=0, y=0)
+            self.nameframe = Frame(self.toptab, bg=rootobj.STYLES["TOP_TAB"], height=32)
+            ttk.Label(self.nameframe, text=rootobj.APPLICATION_NAME, font=(rootobj.STYLES["FONT"], 18)).pack(side=LEFT, padx=(0, 16))
+            Button(self.nameframe, command=lambda: self.rootobj.clear(), text="Clear Text", width=8).pack(side=LEFT)
             self.nameframe.pack(fill=X)
-            self.procedures_tab = Frame(self.toptab, bg="#69db88", height=80)
+            self.procedures_tab = Frame(self.toptab, bg=rootobj.STYLES["TOP_TAB"], height=80)
             self.procedures_tab.pack(fill=X)
             self.toptab.pack(fill=BOTH)
-            for proc in procedures:
-                self.add_procedure(proc)
-        def add_procedure(self, proc_dict):
-            if "name" not in proc_dict or "proc" not in proc_dict:
-                return
+            for classname, classobj in rootobj.CLASSES.items():
+                self.add_procedure(classname, classobj)
+        def add_procedure(self, classname, classobj):
+            if "__icon__" in dir(classobj):
+                if len(classobj.__icon__) > 4 and classobj.__icon__[-4:] == ".png":
+                    icon_path = classobj.__icon__
+                else:
+                    print(f"WARNING: icon path was specified in class '{classname}' but was not a '.png' image. Terminally can only support this image type.")
+                    icon_path = "no_image.png"
+            else:
+                icon_path = "no_image.png"
             proc = Frame(self.procedures_tab, width=64, height=64)
             proc.pack(side=LEFT, pady=12, padx=12)
-            if "icon" not in proc_dict:
-                self.rootobj.images.append(PhotoImage(file="no_image.png"))
-            else:
-                self.rootobj.images.append(PhotoImage(file=proc_dict["icon"]))
-            Button(proc, command=lambda: self.rootobj.load_class(proc_dict["proc"]),
-                   text=proc_dict["name"], image=self.rootobj.images[-1], compound=BOTTOM,
-                   width=64, height=64).pack(side=TOP, fill=BOTH)
+            self.rootobj.images.append(PhotoImage(file=icon_path))
+            Button(proc, command=lambda: self.rootobj.load_class(classname),
+                   text=classname, image=self.rootobj.images[-1], compound=BOTTOM,
+                   width=96, height=64).pack(side=TOP, fill=BOTH)
+        def refresh(self):
+            for widget in self.procedures_tab.winfo_children():
+                widget.destroy()
+            self.rootobj.images = []
+            for classname, classobj in self.rootobj.CLASSES.items():
+                self.add_procedure(classname, classobj)
     class sidebar_class():
         def __init__(self,rootobj):
             self.rootobj = rootobj
-            self.sidebar = Frame(self.rootobj, bg="#a7c0fa", width=234)
+            self.sidebar = Frame(self.rootobj, bg=rootobj.STYLES["SIDEBAR"], width=234)
             self.sidebar.pack(fill=BOTH, side=LEFT, expand=False)
         def add_methods(self,classobj):
             i = 0
             self.rootobj.functions = []
             for widget in self.sidebar.winfo_children():
                 widget.destroy()
+            if "function_info" in dir(classobj):
+                function_info = classobj.function_info
+            else:
+                function_info = {}
             for func_name in dir(classobj):
-                # print(type(getattr(classobj, func_name)))
                 if (len(func_name) > 4 and func_name[0:2] + func_name[-2:] == "____") or (not callable(getattr(classobj, func_name))): # ignore magic methods and non functions
                     continue
+                output_name = func_name
+                if func_name in function_info:
+                    if "HIDDEN" in function_info[func_name]:
+                        if function_info[func_name]["HIDDEN"] == True:
+                            continue
+                    if "NAME" in function_info[func_name]:
+                        output_name = function_info[func_name]["NAME"]
                 self.rootobj.functions.append(getattr(classobj, func_name))
                 func = Frame(self.sidebar,height=16)
                 func.pack()
-                Button(func,command=lambda i=i: self.rootobj.exec_function(i),text=func_name,width=32).pack(side=LEFT,fill=BOTH)
+                Button(func,command=lambda i=i: self.rootobj.exec_function(i),text=output_name,width=32).pack(side=LEFT,fill=BOTH)
                 i += 1
     class maintab_class():
         def __init__(self,rootobj):
@@ -70,36 +72,40 @@ class base_window(Tk):
         def run_function(self,funcobj):
             if self.rootobj.awaiting_user_input:
                 return
+            arg_names = funcobj.__code__.co_varnames[:funcobj.__code__.co_argcount]
             if funcobj.__code__.co_argcount == 0:
                 funcobj()
             else:
-                self.input(funcobj.__code__.co_varnames,funcobj)
+                self.input(arg_names, funcobj)
                 self.rootobj.awaiting_user_input = True
         def output(self,*output_obj):
             if len(output_obj) != 0:
+                print(len(self.statement_queue), self.maintab.winfo_height())
                 for obj in output_obj:
                     for widget in self.maintab.winfo_children():
                         widget.destroy()
-                    print(len(self.statement_queue),self.maintab.winfo_height())
                     while len(self.statement_queue) > self.maintab.winfo_height() // 20:
                         self.statement_queue.pop(0)
-                    self.statement_queue.append(repr(obj))
+                    self.statement_queue.append(str(obj))
                     for smt in self.statement_queue:
                         lbl = ttk.Label(self.maintab,text=smt)
                         lbl.pack(fill=BOTH)
         def submit(self,funcobj):
-            input_list = [x.get() for x in self.button_list]
+            user_input_list = [x.get() for x in self.button_list]
             try:
-                res = funcobj(*input_list)
+                res = funcobj(*user_input_list)
                 if res is not None:
+                    print("true")
                     self.output(repr(res))
             except Exception as e:
                 self.output(repr(e))
             self.rootobj.awaiting_user_input = False
+            self.rootobj.refresh()
+            self.output('')
         def cancel(self):
             self.rootobj.awaiting_user_input = False
-            self.output("Cancel")
-        def input(self,argument_names,funcobj):
+            self.output("Cancelled")
+        def input(self, argument_names, funcobj):
             self.rootobj.awaiting_user_input = True
             self.button_list = []
             for arg in argument_names:
@@ -108,24 +114,53 @@ class base_window(Tk):
                 ent.pack()
                 lbl.pack()
                 self.button_list.append(ent)
-            Button(self.maintab,command=lambda x=funcobj: self.submit(x), text="submit",width=8).pack()
+            Button(self.maintab, command=lambda x=funcobj: self.submit(x), text="submit",width=8).pack()
             Button(self.maintab, command=lambda :self.cancel(), text="cancel", width=8).pack()
-    def __init__(self,application_name,procedures,classes={}):
+    def __init__(self,application_name,classes=None,styles=None):
         super().__init__()
-        self.title(APPLICATION_NAME)
+        self.APPLICATION_NAME = application_name
+        if styles == None:
+            self.STYLES = {"TOP_TAB": "#69db88", "APPLICATION_NAME": "#69db88", "SIDEBAR": "#a7c0fa", "FONT": "Arial"}
+        else:
+            self.STYLES = styles
         self.minsize(800, 500)
         self.images = []
-        self.awaiting_user_input=False
+        self.awaiting_user_input = False
+        self.user_input_arguments = []
         self.functions = []
-        self.classes = classes # {classname as str: classobj}
-        self.toptab = self.toptab_class(self,application_name,procedures)
+        self.CLASSES = {}
+        self.CURRENT_CLASS = None
+        if classes is not None:
+            for classobj in classes:
+                self.add_class(classobj)
+        self.toptab = self.toptab_class(self)
         self.sidebar = self.sidebar_class(self)
         self.maintab = self.maintab_class(self)
+    def add_class(self, classobj):
+        if "__name__" in dir(classobj):
+            nm = classobj.__name__
+        else:
+            nm = type(classobj).__name__
+        if nm not in self.CLASSES:
+            self.CLASSES[nm] = classobj
+        else:
+            print(f"WARNING: Class with name '{nm}' was ignored as this name was already present in loaded classes")
     def load_class(self,classname):
-        if classname not in self.classes:
+        if classname not in self.CLASSES:
             raise LookupError(f"Classname '{classname}' was not found in compiled classes")
-        self.sidebar.add_methods(self.classes[classname])
+        if self.awaiting_user_input:
+            return
+        self.CURRENT_CLASS = self.CLASSES[classname]
+        self.sidebar.add_methods(self.CLASSES[classname])
     def exec_function(self,i):
         self.maintab.run_function(self.functions[i])
-ROOT = base_window(APPLICATION_NAME,PROCEDURES,CLASSES)
-ROOT.mainloop()
+    def refresh(self):
+        if self.awaiting_user_input:
+            return
+        self.toptab.refresh()
+        self.sidebar.add_methods(self.CURRENT_CLASS)
+    def clear(self):
+        if self.awaiting_user_input:
+            return
+        self.maintab.statement_queue = []
+        self.maintab.output('')
